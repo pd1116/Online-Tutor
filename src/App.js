73a96202a305a843058ff933ae8d82b0c2a3383a
Shamuel <shamuelsam300@gmail.com>
2022-01-21 22:54:44 +0600

    file

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Main from './Components/Main/Main';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Button } from '@material-ui/core';
+import Search from './Components/Search/Search';
+
 function App() {
   return (
     <div className="App">
       <Main></Main>
- 
+      <Search></Search>
+    
     </div>
   );
 }
---
+++ src/Components/Search/Search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div className="container p-0">
+            <h1>This is search section </h1>
+
+     
+            <select class="custom-select form-control">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+
+
+        </div>
+    );
+};
+
+export default Search;
