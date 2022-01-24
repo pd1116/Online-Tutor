e76f5d7abc2462fb8af3f75e822a32c913f7a088
Shamuel <shamuelsam300@gmail.com>
2022-01-24 22:03:32 +0600

    service add

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -3,61 +3,69 @@ import './Main.css';
 
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Service from '../Service/Service';
 
 const Main = () => {
     return (
-        <div className="container bg-warning">
+        <div className="container">
+            <div className="container bg-warning ">
 
-        {/* top banner start here  */}
+                {/* top banner start here  */}
 
-        <div class="row mt-1 ">
-            <div class=" col-md-6  text-center ">
-                <h2 >Online Tutor</h2>
-            </div>
-            <div class="col-md-6 ">
+                <div class="row mt-1 ">
+                    <div class=" col-md-6  text-center ">
+                        <h2 >Online Tutor</h2>
+                    </div>
+                    <div class="col-md-6 ">
 
-                <button class="btn btn-sm btn-primary top-btn m-2">Register</button>
-                <button class=" btn btn-sm btn-primary top-btn m-2">Login</button>
+                        <button class="btn btn-sm btn-primary top-btn m-2">Register</button>
+                        <button class=" btn btn-sm btn-primary top-btn m-2">Login</button>
 
-            </div>
-            {/* top banner ends here  */}
+                    </div>
+                    {/* top banner ends here  */}
 
-            {/* Navbar start here  */}
-            <Navbar bg="danger"  expand="sm" className="navbar">
-               
-                    <Navbar.Brand href="#home " >Tutor</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            {/* <Link to="/home" >Home</Link> */}
-                            <Nav.Link href="/home" >Home</Nav.Link>
-                            <Nav.Link href="/search">Search Tutor</Nav.Link>
-                            <NavDropdown title="Features" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Features</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Request for tutor</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Student Dashboard</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Help Forum</NavDropdown.Item>
-                              
-                               
-                            </NavDropdown>
-                            
-                            <Nav.Link href="#link">Classroom</Nav.Link>
-                            <Nav.Link href="#link">Contact Us</Nav.Link>
-                            
-                        </Nav>
-                    </Navbar.Collapse>
-               
-            </Navbar>
-            
-            {/* Navbar ends here  */}
+                    {/* Navbar start here  */}
+                    <Navbar bg="danger" expand="sm" className="navbar">
+
+                        <Navbar.Brand href="#home " >Tutor</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                {/* <Link to="/home" >Home</Link> */}
+                                <Nav.Link href="/home" >Home</Nav.Link>
+                                <Nav.Link href="/search">Search Tutor</Nav.Link>
+                                <NavDropdown title="Features" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Features</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Request for tutor</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Student Dashboard</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.4">Help Forum</NavDropdown.Item>
 
 
+                                </NavDropdown>
+
+                                <Nav.Link href="#link">Classroom</Nav.Link>
+                                <Nav.Link href="#link">Contact Us</Nav.Link>
+
+                            </Nav>
+                        </Navbar.Collapse>
+
+                    </Navbar>
+
+
+
+                </div>
            
 
 
+            </div>
+                 {/* Navbar ends here  */}
+                 
+            {/* service section start here */}
+            <Service></Service>
+            {/* service section ends here */}
+
         </div>
-    </div>
-);
+    );
 };
 
 export default Main;
---
+++ src/Components/Service/Service.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div className="service bg-info py-5">
+          {/* Service section  */}
+
+
+    <div class="container-fluid">
+        <div class="row align-items-center text-center text-white  ">
+            <div class="col-md-6 col-lg-4 d-flex my-3">
+                <i class="fas fa-bomb fa-5x mr-3 "></i>
+                <div class="inner-text">
+                    <h1 class="my-3">Search Tutor</h1>
+                    <p class="lead">Lorem ipsum dolor sit amet cons?Lorem ipsum dolor sit amet cons?</p>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-4 d-flex my-3">
+                <i class="fas fa-spinner fa-5x mr-3 "></i>
+                <div class="inner-text">
+                    <h1 class="my-3">Review Teacher</h1>
+                    <p class="lead">Lorem ipsum dolor sit amet cons?Lorem ipsum dolor sit amet cons?</p>
+                </div>  
+            </div>
+
+            <div class="col-md-6 col-lg-4 d-flex my-3">
+                <i class="fas fa-star fa-5x mr-3 "></i>
+                <div class="inner-text">
+                    <h1 class="my-3">Connect Tutor</h1>
+                    <p class="lead">Lorem ipsum dolor sit amet cons?Lorem ipsum dolor sit amet cons?</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+        </div>
+    );
+};
+
+export default Service;
