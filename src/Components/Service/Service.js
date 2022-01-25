36af8af7369bc1aa226ab9dd52a7c37a405b0a5b
Shamuel <shamuelsam300@gmail.com>
2022-01-25 23:53:48 +0600

    service section updated

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 
 const Service = () => {
     return (
@@ -7,26 +8,31 @@ const Service = () => {
 
 
     <div class="container-fluid">
+
         <div class="row align-items-center text-center text-white  ">
             <div class="col-md-6 col-lg-4 d-flex my-3">
-                <i class="fas fa-bomb fa-5x mr-3 "></i>
+            <i class="fas fa-spinner fa-5x mr-3 "></i>
+
                 <div class="inner-text">
+                <PersonSearchIcon  style={{color:'red',fontSize:'60px'}}></PersonSearchIcon>
                     <h1 class="my-3">Search Tutor</h1>
                     <p class="lead">Lorem ipsum dolor sit amet cons?Lorem ipsum dolor sit amet cons?</p>
                 </div>
             </div>
 
             <div class="col-md-6 col-lg-4 d-flex my-3">
-                <i class="fas fa-spinner fa-5x mr-3 "></i>
+               
                 <div class="inner-text">
+                <PersonSearchIcon  style={{color:'red',fontSize:'60px'}}></PersonSearchIcon>
                     <h1 class="my-3">Review Teacher</h1>
                     <p class="lead">Lorem ipsum dolor sit amet cons?Lorem ipsum dolor sit amet cons?</p>
                 </div>  
             </div>
 
             <div class="col-md-6 col-lg-4 d-flex my-3">
-                <i class="fas fa-star fa-5x mr-3 "></i>
+                {/* <i class="fas fa-star fa-5x mr-3 "></i> */} 
                 <div class="inner-text">
+                <PersonSearchIcon  style={{color:'red',fontSize:'60px'}}></PersonSearchIcon>
                     <h1 class="my-3">Connect Tutor</h1>
                     <p class="lead">Lorem ipsum dolor sit amet cons?Lorem ipsum dolor sit amet cons?</p>
                 </div>
