25eb2665e4f95a70dbe51e589d13fad49c5d40d4
Shamuel <shamuelsam300@gmail.com>
2022-02-11 23:15:19 +0600

    update explore teacher

--- src/Components/ExploreTutor/ExploreTutor.js
+++ src/Components/ExploreTutor/ExploreTutor.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import './ExploreTutor.css'
 
 const ExploreTutor = () => {
     return (
-        <div>
-            <h1>Explore teacher</h1>
-            <div className="btn btn-primary m-5 p-5">As a Student</div>
-            <div className="btn btn-primary p-5">As a Teacher</div>
+        <div className="bg-danger container">
+            <h1 className="py-3">Explore teacher</h1>
 
+
+            <div className="Explore">
+
+                <div className="join">
+                    <h3><span >Join as a </span>STUDENT</h3>
+                    <ul>
+                        <li>Search for online and local tutors</li>
+
+                        <li>Communicate with any of our tutors for free</li>
+                        <li>Manage your lessons through our bookings system</li>
+                        <li>If you are not satisfied with your tutor we will refund our fee</li>
+                    </ul>
+                    <div class="joinbtn"><a class="btn btn-primary " href="#">REGISTER</a></div>
+                </div>
+
+
+                        {/* //teacher */}
+                <div className="join">
+                    <h3><span >Join as a </span>TUTOR</h3>
+                    <ul>
+                        <li>Search for online and local tutors</li>
+
+                        <li>Communicate with any of our tutors for free</li>
+                        <li>Manage your lessons through our bookings system</li>
+                        <li>If you are not satisfied with your tutor we will refund our fee</li>
+                    </ul>
+                    <div class="joinbtn"><a class="btn btn-outline-danger " href="#">REGISTER</a></div>
+                </div>
+
+            </div>
+             <section class="sectionnGap"></section>
 
         </div>
     );
