ed05a504c1f7a1e263de1ea61571b368e5f02156
Shamuel <shamuelsam300@gmail.com>
2022-03-01 01:21:44 +0600

    updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,10 +3,11 @@ import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import AddIcCallIcon from '@mui/icons-material/AddIcCall';
+// import AddIcCallIcon from '@mui/icons-material/AddIcCall';
 import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
-
 import { Link } from 'react-router-dom';
+
+// import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
@@ -20,13 +21,13 @@ const Footer = () => {
                     <a href="http://twitter.com/"><InstagramIcon className="text-danger"></InstagramIcon></a>
                     
                     </div>
-                    <p>&nbsp;©Copyright {(new Date).getFullYear()}, onlinetutor.com : All Rights Reserved.</p>
+                    <p>&nbsp;©Copyright {(new Date()).getFullYear()}, onlinetutor.com : All Rights Reserved.</p>
                     <p>Privacy Policy&nbsp;&nbsp;Terms of Service &nbsp;&nbsp; </p>
                 </div>
                 <div className="col-md-4 col-sm-12">
                 <div className="social pb-3">
                     <h3>Customer Service</h3>
-                   <a href="#" > <WhatsAppIcon className="text-success"></WhatsAppIcon></a> <h5>01303001354</h5>
+                   <Link href="#" > <WhatsAppIcon className="text-success"></WhatsAppIcon></Link> <h5>01303001354</h5>
                     <h6>How it works?</h6>
                     <h6>FAQ</h6>
 
